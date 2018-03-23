d7ccc67ab64bb2271902602b76b83fcd4b029be2
JasonTwigg <jasontwigg1@gmail.com>
2018-03-22 17:23:24 -0700

    Professional Commit Message

---
+++ app/src/test/java/edu/up/cs301/pusoydos/SJLocalGameTest.java
@@ -0,0 +1,31 @@
+package edu.up.cs301.pusoydos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jason on 3/22/2018.
+ */
+public class SJLocalGameTest {
+    @Test
+    public void checkIfGameOver() throws Exception {
+
+    }
+
+    @Test
+    public void sendUpdatedStateTo() throws Exception {
+
+    }
+
+    @Test
+    public void canMove() throws Exception {
+
+    }
+
+    @Test
+    public void makeMove() throws Exception {
+
+    }
+
+}
