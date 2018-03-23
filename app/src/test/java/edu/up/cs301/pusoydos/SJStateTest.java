4591808c91bca20ac0e70d2eaee4b68b404865b7
JasonTwigg <jasontwigg1@gmail.com>
2018-03-22 17:26:26 -0700

    Professional Commit Message

---
+++ app/src/test/java/edu/up/cs301/pusoydos/SJStateTest.java
@@ -0,0 +1,106 @@
+package edu.up.cs301.pusoydos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jason on 3/22/2018.
+ */
+public class SJStateTest {
+    @Test
+    public void getDeck() throws Exception {
+
+    }
+
+    @Test
+    public void toPlay() throws Exception {
+
+    }
+
+    @Test
+    public void setToPlay() throws Exception {
+
+    }
+
+    @Test
+    public void nullAllButTopOf2() throws Exception {
+
+    }
+
+    @Test
+    public void selectCard() throws Exception {
+
+    }
+
+    @Test
+    public void playCard() throws Exception {
+
+    }
+
+    @Test
+    public void passAction() throws Exception {
+
+    }
+
+    @Test
+    public void changeTurn() throws Exception {
+
+    }
+
+    @Test
+    public void canPlay() throws Exception {
+
+    }
+
+    @Test
+    public void getPiles() throws Exception {
+
+    }
+
+    @Test
+    public void setPiles() throws Exception {
+
+    }
+
+    @Test
+    public void getPileSizes() throws Exception {
+
+    }
+
+    @Test
+    public void setPileSizes() throws Exception {
+
+    }
+
+    @Test
+    public void getTurnNum() throws Exception {
+
+    }
+
+    @Test
+    public void setTurnNum() throws Exception {
+
+    }
+
+    @Test
+    public void getPerspective() throws Exception {
+
+    }
+
+    @Test
+    public void setPerspective() throws Exception {
+
+    }
+
+    @Test
+    public void getCardsSelected() throws Exception {
+
+    }
+
+    @Test
+    public void setCardsSelected() throws Exception {
+
+    }
+
+}
